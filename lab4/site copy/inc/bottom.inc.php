f9f34bdf81f3113eb7ce54175e4d9851eaf16a66
Dima Kozlov <116185824+DikozWorld@users.noreply.github.com>
2025-12-23 07:15:23 +0000

    save lab4

---
+++ lab4/site copy/inc/bottom.inc.php
@@ -0,0 +1,4 @@
+<?php
+$currentYear = getdate()['year'];
+echo "&copy; Супер Мега Веб-мастер, 2000 &ndash; " . $currentYear;
+?>
---
+++ lab4/site copy/inc/data.inc.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+$cols = $_GET['cols'] ?? 5;
+$rows = $_GET['rows'] ?? 5;
+$color = $_SESSION['color'] ?? '#00ff88ff';
+
+$menu = [
+    ['link' => 'Домой', 'href' => 'index.php'],
+    ['link' => 'О нас', 'href' => 'index.php?id=about'],
+    ['link' => 'Контакты', 'href' => 'index.php?id=contact'],
+    ['link' => 'Таблица умножения', 'href' => 'index.php?id=table'],
+    ['link' => 'Калькулятор', 'href' => 'index.php?id=calc'],
+];
